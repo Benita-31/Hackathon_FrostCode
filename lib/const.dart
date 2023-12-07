d58e19e38d7d3a0cc326c4bc6e8a41085ff1b917
Benita-31 <benita.elearn@gmail.com>
2023-12-07 12:38:26 +0530

    initial commit

---
+++ lib/const.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var backgroundColor=Color.fromARGB(255, 234, 240, 245);
---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hackathon/models/coffee.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_hackathon/models/coffee_shop.dart';
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child)=> SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+        children: [const Text("How would you like your coffee?", 
+        style: TextStyle(fontSize: 20),),
+        const SizedBox(height: 25),
+
+        Expanded(
+          child: ListView.builder(
+          itemCount: value.coffeeShop.length,
+          itemBuilder: (context, index)
+        {
+            Coffee eachCoffee = value.coffeeShop[index];
+            return Text(eachCoffee.name,);
+           },
+           ),
+          ),
+         ],
+        ),
+      ),
+    ),
+    );
+  }
+}
